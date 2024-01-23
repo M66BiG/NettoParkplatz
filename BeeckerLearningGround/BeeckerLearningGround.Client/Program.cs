72cf3cd6c2fd9e742076869c49c803e1a8c00c70
Melih aus Beeck <melihesad@hotmail.de>
2024-01-23 21:55:24 +0100

    Last Version working

--- BeeckerLearningGround/BeeckerLearningGround.Client/Program.cs
+++ BeeckerLearningGround/BeeckerLearningGround.Client/Program.cs
@@ -2,9 +2,9 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-//builder.Services.AddScoped(sp => new HttpClient
-//{
-//    BaseAddress = new Uri("https://jsonplaceholder.typicode.com/")
-//});
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri("https://jsonplaceholder.typicode.com/")
+});
 
 await builder.Build().RunAsync();
