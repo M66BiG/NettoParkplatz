7fe8579d9eb63c1c3f0dc2972b8f037c1926fd71
Melih aus Beeck <melihesad@hotmail.de>
2024-01-24 23:56:06 +0100

    implemented more requests on requesting.razor (complex users)

--- BeeckerLearningGround/BeeckerLearningGround.Client/Models/RequestModel.cs
+++ BeeckerLearningGround/BeeckerLearningGround.Client/Models/RequestModel.cs
@@ -7,4 +7,36 @@ public class RequestModel
     public string title { get; set; }
     public string body { get; set; }
 }
+public class UserModel
+{
+    public int id { get; set; }
+    public string name { get; set; }
+    public string username { get; set; }
+    public string email { get; set; }
+    public Address address { get; set; }
+    public string phone { get; set; }
+    public string website { get; set; }
+    public Company company { get; set; }
+}
 
+public class Address
+{
+    public string street { get; set; }
+    public string suite { get; set; }
+    public string city { get; set; }
+    public string zipcode { get; set; }
+    public Geo geo { get; set; }
+}
+
+public class Geo
+{
+    public string lat { get; set; }
+    public string lng { get; set; }
+}
+
+public class Company
+{
+    public string name { get; set; }
+    public string catchPhrase { get; set; }
+    public string bs { get; set; }
+}
