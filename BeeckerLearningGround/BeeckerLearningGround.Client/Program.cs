e041f7291d7e6322ae0c85846ec898a3ef647711
Melih aus Beeck <melihesad@hotmail.de>
2024-01-24 04:58:19 +0100

    Added things from Master

--- BeeckerLearningGround/BeeckerLearningGround.Client/Program.cs
+++ BeeckerLearningGround/BeeckerLearningGround.Client/Program.cs
@@ -3,4 +3,9 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
+builder.Services.AddHttpClient("JsonPlaceholder", client =>
+{
+    client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com/");
+});
+
 await builder.Build().RunAsync();
